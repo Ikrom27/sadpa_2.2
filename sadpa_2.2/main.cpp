5d50a42a4e41785cae44702ed6140d6beb5ee0a5
Ikrom <ikrom.hasanbaev.27@gmail.com>
2022-09-26 18:48:58 +0300

    fuuuuuck

--- sadpa_2.2/main.cpp
+++ sadpa_2.2/main.cpp
@@ -1,93 +1,125 @@
-﻿#include <iostream>
-#include "fstream"
+﻿#include "fstream"
 #include "iostream"
 #include "istream"
 #include <string>
 
 
 struct book {
-    std::string group;
-    std::string discipline;
-    int lesson_num;
-    int week_num;
-    int week_day;
-    std::string lesson_type;
-    std::string audience_number;
+	std::string group;
+	std::string discipline;
+	int lesson_num;
+	int week_num;
+	int week_day;
+	std::string lesson_type;
+	std::string audience_number;
 };
 
 
 void create_bin_file(std::ifstream& ft, std::ofstream& fb)
 {
-    book file;
-
-    while (!ft.eof())
-    {
-        getline(ft, file.group);
-        getline(ft, file.discipline);
-        ft >> file.lesson_num;
-        ft >> file.week_num;
-        ft >> file.week_day;
-        getline(ft, file.lesson_type);
-        getline(ft, file.audience_number);
-        fb.write((char*)&file, sizeof(book));
-        fb.clear();
-    } 
-    ft.close();
-    fb.close();
+	book file;
+	while (!ft.eof())
+	{
+		getline(ft, file.group);
+		getline(ft, file.discipline);
+		ft >> file.lesson_num;
+		ft >> file.week_num;
+		ft >> file.week_day;
+		getline(ft, file.lesson_type);
+		getline(ft, file.audience_number);
+		fb.write((char*)&file, sizeof(book));
+		fb.clear();
+	}
+	ft.close();
+	fb.close();
 }
 
 
 void out_bin_file(std::ifstream& fb)
 {
-    book file;
-    //чтение из файла всей записи 
-    fb.read((char*)&file, sizeof(book));
+	book file;
+	
+	fb.read((char*)&file, sizeof(book));
+	
+	std::cout << file.group << std::endl;
+	std::cout << file.discipline << std::endl;
+	std::cout << file.lesson_num << std::endl;
+	std::cout << file.week_num << std::endl;
+	std::cout << file.week_day << std::endl;
+	std::cout << file.lesson_type << std::endl;
+	std::cout << file.audience_number << std::endl;
 
-    std::cout << file.group << std::endl;
-    std::cout << file.discipline << std::endl;
-    std::cout << file.lesson_num << std::endl;
-    std::cout << file.week_num << std::endl;
-    std::cout << file.week_day << std::endl;
-    std::cout << file.lesson_type << std::endl;
-    std::cout << file.audience_number << std::endl;
-    
-    fb.read((char*)&file, sizeof(book));
-    fb.clear();
-    fb.seekg(0);
-    if (!fb.good()) {
-        std::cout << "Произошла ошибка out_bin_file : !fb.good()" << std::endl;
-    }
-    fb.close();
+	//fb.read((char*)&file, sizeof(book));
 }
 
+void add_bin_file(char* fnameBin)
+{
+	book x;
+	std::ofstream fadd(fnameBin, std::ios::in | std::ios::binary | std::ios::app);
 
+
+
+	fadd.write((char*)&x, sizeof(book));
+	fadd.close();
+}
 int main()
 {
-    setlocale(LC_ALL, "ru");
+	setlocale(LC_ALL, "rus");
 
-    //char fnameText[30], fnameBin[30];
+	std::ifstream ft;
+	std::ofstream fb;
 
-    //std::cout << "Name for Text: "; std::cin >> fnameText;
-    //std::cout << "Name for Text: "; std::cin >> fnameBin;
+	//char fnameText[30], fnameBin[30];
 
-    std::ifstream ft("binar.txt", std::ios::binary);
-    std::ofstream fb("binar.dat", std::ios::out | std::ios::binary);
+	char fnameText[30] = "binar.txt";
+	char fnameBin[30] = "binar.dat";
+	
+	//std::cout << "Name for Text "; std::cin >> fnameText;
+	//std::cout << "Name for Text "; std::cin >> fnameBin;
+	
+	ft.open(fnameText, std::ios::out);
+	fb.open(fnameBin, std::ios::out | std::ios::binary);
+	std::ifstream fbb(fnameBin, std::ios::in | std::ios::binary);
+	if (!ft || !fb)
+	{
+		std::cout << "файл не открыт";
+		return 1;
+	}
 
-    //std::ifstream ft("text.txt");
-    //std::cout << ft.rdbuf();
+	int num;
 
-    //std::ofstream fb("binar.dat", std::ios::out | std::ios::binary);
+	while (1)
+	{
+		std::cout << " Operation for files" << std::endl;
+		std::cout << " 1. Create for TextFile inBinFile" << std::endl;
+		std::cout << " 2. OUT for BinFile" << std::endl;
+		std::cout << " 3. Add record in BinFile" << std::endl;
+		std::cout << " 4. EXIT" << std::endl;
+		std::cout << "numPunkt= "; std::cin >> num;
 
-    if (!ft || !fb)
-    {
-        std::cout << "файл не открыт";
-        return 1;
-    }
+		switch (num)
+		{
+		case 1: create_bin_file(ft, fb); break;
+		case 2: 
+			{
+			std::ifstream fbb(fnameBin, std::ios::in | std::ios::binary);
+			out_bin_file(fbb); break;
+			if (!fb)
+			{
+				std::cout << "файл не открыт";
+				system("pause");
+			}
+			fb.close();
+			}
+		case 3: add_bin_file(fnameBin); break;
+		case 4: exit(0);
+		}
 
-    std::cout << ft.rdbuf();
-
-    create_bin_file(ft, fb);
-    std::ifstream fbb("binar.dat", std::ios::in | std::ios::binary);
-    out_bin_file(fbb);
-    return 0;
+		if(!fbb.good())
+		{
+			std::cout << "Ошибка ввода" << std::endl;
+			return 1;
+		}
+	}
+	return 0;
 }
